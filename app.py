9560d75d04d7a509b0c8e16e6ec2f120a03fdbd6
Tyler <gosselin.code@gmail.com>
2021-03-25 14:46:28 -0600

    Fix DB

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 from datetime import timedelta
 from flask import Flask, json, request, jsonify, session
 from flask.globals import session
-from flask_marshmallow.schema import Schema
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
@@ -20,34 +19,36 @@ flask_bcrypt = Bcrypt(app)
 
 
 class User(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(45), nullable=False)
-    user_email = db.Column(db.String(100), nullable=False)
-    user_password = db.Column(db.String(45), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(45), unique = False, nullable=False)
+    email = db.Column(db.String(100), unique = False,  nullable=False)
+    password = db.Column(db.String(45), unique = False, nullable=False)
 
-    def __init__(self, user_name, user_email, user_password):
-        self.user_name = user_name
-        self.user_email = user_email
-        self.user_password = user_password
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
 
 
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ("user_id", "user_name", "user_emai", "user_password")
+        fields = ("id", "username", "email", "password")
 
 
 class Blog(db.Model):
-    __tablename__ = "blogs"
-    blog_id = db.Column(db.Integer, primary_key=True)
-    blog_title = db.Column(db.String(100), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique = False,  nullable=False)
+    author = db.Column(db.String(40), unique = False,  nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    def __init__(self, blog_title):
-        self.blog_title = blog_title
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
 
 
 class BlogSchema(ma.Schema):
     class Meta:
-        fields = ("blog_id", "blog_title")
+        fields = ("id", "title", "author")
 
 
 user_schema = UserSchema()
@@ -61,67 +62,77 @@ def hello():
     return "Hello World"
 
 
-@app.route('/api/v1/register-user', methods=['POST'])
+@app.route('/api/register-user', methods=['POST'])
 def register():
     post_data = request.get_json()
-    user_name = post_data.get('user_name')
-    user_email = post_data.get('user_email')
-    user_password = post_data.get('user_password')
+    username = post_data.get('username')
+    email = post_data.get('email')
+    password = post_data.get('password')
     hashed_password = flask_bcrypt.generate_password_hash(
-        user_password).decode('utf-8')
-    new_user = User(user_name, user_email, user_password=hashed_password)
+        password).decode('utf-8')
+    new_user = User(username, email, password = hashed_password)
     db.session.add(new_user)
     db.session.commit()
     session.permanent = True
-    session['user_name'] = user_name
+    session['username'] = username
     print(session)
     return jsonify(user_schema.dump(new_user))
 
 
-@app.route('/api/v1/get-users')
+@app.route('/api/get-users')
 def get_users():
     all_users = User.query.all()
     return jsonify(users_schema.dump(all_users))
 
 
-@app.route('/api/v1/create-blog', methods=['POST'])
+@app.route('/api/create-blog', methods=['POST'])
 def create_blog():
     post_data = request.get_json()
-    blog_title = post_data.get('blog_title')
-    new_blog = Blog(blog_title)
+    title = post_data.get('title')
+    author = post_data.get('author')
+    new_blog = Blog(title, author)
     db.session.add(new_blog)
     db.session.commit()
     return jsonify(blog_schema.dump(new_blog))
 
 
-@app.route('/api/v1/get-blogs')
+@app.route('/api/get-blogs')
 def get_blogs():
     all_blogs = Blog.query.all()
     return jsonify(blogs_schema.dump(all_blogs))
 
 
-@app.route('/api/v1/login', methods=['POST'])
+@app.route('/api/edit-blog/<blog_id>', methods=['PATCH'])
+def edit_blog(blog_id):
+    blog = Blog.query.filter_by(id=blog_id).first()
+    blog.title = request.json.get('title')
+    blog.author = request.json.get('author')
+    db.session.commit()
+    return jsonify(blog_schema.dump(blog))
+
+
+@app.route('/api/login', methods=['POST'])
 def login():
     post_data = request.get_json()
     db_user = User.query.filter_by(
-        user_name=post_data.get('user_name')).first()
+        username=post_data.get('username')).first()
     if db_user is None:
         return "User NOT Found", 401
-    password = post_data.get('user_password')
-    db_user_hashed_password = db_user.user_password
+    password = post_data.get('password')
+    db_user_hashed_password = db_user.password
     valid_password = flask_bcrypt.check_password_hash(
         db_user_hashed_password, password)
     if valid_password:
         session.permanent = True
-        session['user_name'] = post_data.get('user_name')
+        session['username'] = post_data.get('username')
         return jsonify('User Verified')
     return "Password Invalid", 401
 
 
-@app.route('/api/v1/logged-in')
+@app.route('/api/logged-in')
 def logged_in():
-    if 'user_name' in session:
-        db_user = User.query.filter_by(user_name=session['user_name']).first()
+    if 'username' in session:
+        db_user = User.query.filter_by(username=session['username']).first()
         if db_user:
             return jsonify('User Logged in Via Cookie')
         else:
@@ -130,9 +141,9 @@ def logged_in():
         return jsonify('Not logged in')
 
 
-@app.route('/api/v1/delete-user/<id>', methods=['DELETE'])
+@app.route('/api/delete-user/<id>', methods=['DELETE'])
 def delete_user(id):
-    user = User.query.filter_by(user_id=id).first()
+    user = User.query.filter_by(id=id).first()
     if user:
         db.session.delete(user)
         db.session.commit()
@@ -140,20 +151,19 @@ def delete_user(id):
     return "User NOT Found", 404
 
 
-@app.route('/api/v1/delete-blog/<id>', methods=['DELETE'])
+@app.route('/api/delete-blog/<id>', methods=['DELETE'])
 def delete_blog(id):
-  blog = Blog.query.filter_by(blog_id = id). first()
-  if blog:
-    db.session.delete(blog)
-    db.session.commit()
-    return jsonify('Blog Deleted')
-  return "Blog Not Found", 404
+    blog = Blog.query.filter_by(id=id). first()
+    if blog:
+        db.session.delete(blog)
+        db.session.commit()
+        return jsonify('Blog Deleted')
+    return "Blog Not Found", 404
 
 
-@app.route('/api/v1/logout', methods=['POST'])
+@app.route('/api/logout', methods=['POST'])
 def logout():
     session.clear()
-    #session.pop('username', None)
     return jsonify('Logged Out')
 
 
